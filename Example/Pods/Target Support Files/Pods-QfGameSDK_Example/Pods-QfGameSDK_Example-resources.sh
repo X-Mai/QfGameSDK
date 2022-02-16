913a67ac9130bbce2586e32995c9d632e46fb168
bomaimai <1006052895@qq.com>
2022-02-16 10:49:09 +0800

    demo update podfile

--- Example/Pods/Target Support Files/Pods-QfGameSDK_Example/Pods-QfGameSDK_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-QfGameSDK_Example/Pods-QfGameSDK_Example-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/../../QfGameSDK/Assets/DJSDK2.0.bundle"
+  install_resource "${PODS_ROOT}/QfGameSDK/QfGameSDK/Assets/DJSDK2.0.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/../../QfGameSDK/Assets/DJSDK2.0.bundle"
+  install_resource "${PODS_ROOT}/QfGameSDK/QfGameSDK/Assets/DJSDK2.0.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
